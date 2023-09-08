8322f3ae0d43a5f162a91f03c449b728589e4581
kristiangronli <36628496+kristiangronli@users.noreply.github.com>
2023-09-08 12:25:19 +0200

    update

--- boilerplate.php
+++ boilerplate.php
@@ -20,7 +20,7 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function ffarticle_block_init() {
+function create_block_boilerplate_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'ffarticle_block_init' );
+add_action( 'init', 'create_block_boilerplate_block_init' );
