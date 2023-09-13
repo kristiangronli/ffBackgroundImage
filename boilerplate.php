356f0b7da69b13c4f5028bbeedf1c89be5992e16
kristiangronli <36628496+kristiangronli@users.noreply.github.com>
2023-09-13 15:38:20 +0200

    Redo

--- boilerplate.php
+++ boilerplate.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name:       FacefirstLogo
- * Description:       Lagre og arranger logoer med tilhørende linker
+ * Plugin Name:       Boilerplate
+ * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Kristian Grønli
+ * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       boilerplate
  *
- * @package           fflogos
+ * @package           ffarticle
  */
 
 /**
@@ -20,7 +20,7 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function create_block_fflogos_block_init() {
+function create_block_boilerplate_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'create_block_fflogos_block_init' );
+add_action( 'init', 'create_block_boilerplate_block_init' );
